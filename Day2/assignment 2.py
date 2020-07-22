2466c37fadca2852acf5a53f109c659e19159e57
Saurabh Singh <68112187+shivanshthakur4u@users.noreply.github.com>
2020-07-22 09:55:09 +0530

    Add files via upload

---
+++ Day2/assignment 2.py
@@ -0,0 +1,32 @@
+
+
+#Variables: Syntax: Variable Name = value
+
+number = 1947
+
+#Operators and Assignment Operation and also Formatted Strings
+
+operand1 = 25
+operand2 = 5
+operation = 0
+
+#Addition
+operation = operand1 + operand2
+print(f"Addition Value = {operation} \n") #Escape Sequences
+#Subtraction
+operation = operand1 - operand2
+print(f"Subtracted Value = {operation}")
+#Division
+operation = operand1//operand2 #Returns Integer
+print(f"Divided Value = {operation}")
+#Reminder
+operation = operand1 % operand2
+print(f'Reminder\'s = {operation}') #Back Slash
+#Multipliaction
+operation = operand1 * operand2
+print(f"Multiplied Value = {operation}")
+
+# Triple Quotes
+print (""" Learning From
+            Let's Upgrade Is Fun !!!!
+                    Thanks A Lot for such an Amazing Platform 😎😀😍🤩""")
